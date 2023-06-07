35cc47b5554e6fc09a94421c20b77d2cc2c93b73
MotasemZiad <motasemabunima@gmail.com>
2023-06-07 13:42:02 +0300

    some updates

--- src/app/products/components/product-details/product-detail.component.spec.ts
+++ src/app/products/components/product-details/product-detail.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductsDetailsComponent } from './product-details.component';
+import { ProductDetailsComponent } from './product-details.component';
 
-describe('ProductsDetailsComponent', () => {
-  let component: ProductsDetailsComponent;
-  let fixture: ComponentFixture<ProductsDetailsComponent>;
+describe('ProductDetailsComponent', () => {
+  let component: ProductDetailsComponent;
+  let fixture: ComponentFixture<ProductDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ProductsDetailsComponent],
+      declarations: [ProductDetailsComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(ProductsDetailsComponent);
+    fixture = TestBed.createComponent(ProductDetailsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
