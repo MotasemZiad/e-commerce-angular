597757d755b97984e7f4efe12b1bec8a4c0ad74c
MotasemZiad <motasemabunima@gmail.com>
2023-06-18 15:13:25 +0300

    add admin module

--- src/app/admin/cart/cart.module.ts
+++ src/app/admin/cart/cart.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CartComponent],
+  imports: [CommonModule],
 })
-export class CartModule { }
+export class CartModule {}
